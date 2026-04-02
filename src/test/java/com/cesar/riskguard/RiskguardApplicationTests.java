293827c193552707ef92396db5678e68ae27a3e4
QualyFerrer <97cesarferrer@gmail.com>
2026-04-02 16:55:10 -0300

    chore: initial project setup with Spring Initializr

---
+++ src/test/java/com/cesar/riskguard/RiskguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.riskguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
